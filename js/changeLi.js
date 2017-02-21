c2bcd4a2706add60a4f4f934bbcec90e4105e3b4
CHEN <chen@MacBook-Pro.local>
2017-02-21 12:07:28 +0800

    12

--- js/changeLi.js
+++ js/changeLi.js
@@ -27,33 +27,288 @@ function creatP(el) {
 function render(el) {
     var images = {
         shanghai: [{
-            title: '上海淮海路',
-            url: 'http://odj13j5fo.bkt.clouddn.com/%E4%B8%8A%E6%B5%B7%E6%B7%AE%E6%B5%B7%E8%B7%AF.jpg'
+            title: 'shanghai1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai1.jpg'
+        },{
+            title: 'shanghai2',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai2.jpg'
+        },{
+            title: 'shanghai3',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai3.jpg'
+        },{
+            title: 'shanghai4',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai4.jpg'
+        },{
+            title: 'shanghai5',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai5.jpg'
+        },{
+            title: 'shanghai6',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai6.jpg'
+        },{
+            title: 'shanghai7',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai7.jpg'
+        },{
+            title: 'shanghai8',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai8.jpg'
+        },{
+            title: 'shanghai9',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai9.jpg'
+        },{
+            title: 'shanghai10',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai10.jpg'
+        },{
+            title: 'shanghai11',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai11.jpg'
+        },{
+            title: 'shanghai12',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai12.jpg'
+        },{
+            title: 'shanghai13',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai13.jpg'
+        },{
+            title: 'shanghai14',
+            url: 'http://odj13j5fo.bkt.clouddn.com/shanghai14.jpg'
         }, ],
         beijing: [{
-            title: '华丰胡同',
-            url: 'http://odj13j5fo.bkt.clouddn.com/%E5%8D%8E%E4%B8%B0%E8%83%A1%E5%90%8C.jpg'
+            title: 'beijing1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing1.jpg'
+        },{
+            title: 'beijing2',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing2.jpg'
+        },{
+            title: 'beijing3',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing3.jpg'
+        },{
+            title: 'beijing4',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing4.jpg'
+        },{
+            title: 'beijing5',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing5.jpg'
+        },{
+            title: 'beijing6',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing6.jpg'
+        },{
+            title: 'beijing7',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing7.jpg'
+        },{
+            title: 'beijing8',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing8.jpg'
+        },{
+            title: 'beijing9',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing9.jpg'
+        },{
+            title: 'beijing10',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing10.jpg'
+        },{
+            title: 'beijing11',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing11.jpg'
+        },{
+            title: 'beijing12',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing12.jpg'
+        },{
+            title: 'beijing13',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing13.jpg'
+        },{
+            title: 'beijing14',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing14.jpg'
+        },{
+            title: 'beijing17',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing17.jpg'
+        },{
+            title: 'beijing18',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing18.jpg'
+        },{
+            title: 'beijing19',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing19.jpg'
+        },{
+            title: 'beijing20',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing20.jpg'
+        },{
+            title: 'beijing21',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing21.jpg'
+        },{
+            title: 'beijing22',
+            url: 'http://odj13j5fo.bkt.clouddn.com/beijing22.jpg'
         }, ],
         suzhou: [{
-            title: '苏州',
+            title: 'suzhou1',
             url: 'http://odj13j5fo.bkt.clouddn.com/suzhou1.JPG'
-        }],
+        },{
+            title: 'suzhou2',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou2.jpg'
+        },{
+            title: 'suzhou3',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou3.jpg'
+        },{
+            title: 'suzhou4',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou4.jpg'
+        },{
+            title: 'suzhou5',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou5.jpg'
+        },{
+            title: 'suzhou6',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou6.jpg'
+        },{
+            title: 'suzhou7',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou7.jpg'
+        },{
+            title: 'suzhou8',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou8.jpg'
+        },{
+            title: 'suzhou9',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou9.jpg'
+        },{
+            title: 'suzhou10',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou10.jpg'
+        },{
+            title: 'suzhou11',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suzhou11.jpg'
+        },],
         tianjin: [{
-            title: '武清站',
-            url: 'http://odj13j5fo.bkt.clouddn.com/%E6%AD%A6%E6%B8%85%E7%AB%992.jpg'
+            title: 'tianjin1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/tianjin1.jpg'
         }],
         people: [{
-            title: '老王',
-            url: 'http://odj13j5fo.bkt.clouddn.com/%E8%80%81%E7%8E%8B.jpg'
-        }],
+            title: 'people1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people1.jpg'
+        },{
+            title: 'people3',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people3.jpg'
+        },{
+            title: 'people4',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people4.jpg'
+        },{
+            title: 'people5',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people5.jpg'
+        },{
+            title: 'people6',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people6.jpg'
+        },{
+            title: 'people7',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people7.jpg'
+        },{
+            title: 'people8',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people8.jpg'
+        },{
+            title: 'people9',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people9.jpg'
+        },{
+            title: 'people10',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people10.jpg'
+        },{
+            title: 'people11',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people11.jpg'
+        },{
+            title: 'people12',
+            url: 'http://odj13j5fo.bkt.clouddn.com/people12.jpg'
+        },],
         life: [{
-            title: '老王',
-            url: 'http://odj13j5fo.bkt.clouddn.com/%E8%80%81%E7%8E%8B.jpg'
-        }],
+            title: 'life2',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life2.jpg'
+        },{
+            title: 'life1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life1.jpg'
+        },{
+            title: 'life3',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life3.jpg'
+        },{
+            title: 'life4',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life4.jpg'
+        },{
+            title: 'life5',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life5.jpg'
+        },{
+            title: 'life6',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life6.jpg'
+        },{
+            title: 'life7',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life7.jpg'
+        },{
+            title: 'life8',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life8.jpg'
+        },{
+            title: 'life9',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life9.jpg'
+        },{
+            title: 'life10',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life10.jpg'
+        },{
+            title: 'life11',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life11.jpg'
+        },{
+            title: 'life12',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life12.jpg'
+        },{
+            title: 'life13',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life13.jpg'
+        },{
+            title: 'life14',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life14.jpg'
+        },{
+            title: 'life15',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life15.jpg'
+        },{
+            title: 'life16',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life16.jpg'
+        },{
+            title: 'life17',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life17.jpg'
+        },{
+            title: 'life18',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life18.jpg'
+        },{
+            title: 'life19',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life19.jpg'
+        },{
+            title: 'life20',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life20.jpg'
+        },{
+            title: 'life21',
+            url: 'http://odj13j5fo.bkt.clouddn.com/life21.jpg'
+        },],
         heyangallery: [{
             title: '故宫',
             url: 'http://odj13j5fo.bkt.clouddn.com/WechatIMG96.jpeg'
-        }]
+        }],
+        Suizhou: [{
+            title: 'suizhou1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suizhou1.jpg'
+        },{
+            title: 'suizhou2',
+            url: 'http://odj13j5fo.bkt.clouddn.com/suizhou2.jpg'
+        }],
+        Kunshan: [{
+            title: 'kunshan1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan1.jpg'
+        },{
+            title: 'kunshan2',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan2.jpg'
+        },{
+            title: 'kunshan3',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan3.jpg'
+        },{
+            title: 'kunshan4',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan4.jpg'
+        },{
+            title: 'kunshan5',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan5.jpg'
+        },{
+            title: 'kunshan6',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan6.jpg'
+        },{
+            title: 'kunshan7',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan7.jpg'
+        },{
+            title: 'kunshan8',
+            url: 'http://odj13j5fo.bkt.clouddn.com/kunshan8.jpg'
+        },],
+        Wuxi: [{
+            title: 'wuxi1',
+            url: 'http://odj13j5fo.bkt.clouddn.com/wuxi1.jpg'
+        },{
+            title: 'wuxi2',
+            url: 'http://odj13j5fo.bkt.clouddn.com/wuxi2.jpg'
+        },]
     }
     var contenter = document.getElementById('contenter');
     //获得data_target属性的值
